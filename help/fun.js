3df03a7461184b95c1a385cedc5a10d1ca5881eb
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-19 00:39:52 +0100

    Update fun.js

--- help/fun.js
+++ help/fun.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
   let embed = new Discord.RichEmbed()
 .setColor(message.guild.me.highestRole.color) 
 .setAuthor(message.author.username+`#${message.author.discriminator}`,message.author.displayAvatarURL)
-.addField(":circus_tent: fun commands 👏" , "catgif, fox, cat, garo, rabbits, dog, shit, slap, gifslap, hug, punch, kill, anime, poke, cuddle, pat, kiss, tickle, memes, ask, otaku, any, gay, nik")
+.addField(":circus_tent: fun commands 👏" , "catgif, fox, cat, garo, rabbits, dog, how gay, rip, shit, slap, gifslap, hug, punch, kill, anime, poke, cuddle, pat, kiss, tickle, memes, ask, otaku, any, gay, nik")
 .setFooter(`if you want more information say -helpfun`)
 .setTimestamp()
   message.channel.sendEmbed(embed);
