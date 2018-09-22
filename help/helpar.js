3355fe9e0d6e49f90b02e62a0713cf4c56e3b782
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-22 22:03:23 +0100

    Update helpar.js

--- help/helpar.js
+++ help/helpar.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
-if(!message.channel.guild) return message.reply('this command is for guilds only!');
+        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
 message.delete()
 let customemoji6 = client.emojis.find(r => r.name === '65654854847848947894');
 let customemoji1 = client.emojis.find(r => r.name === '5449874894189564');
