5535f67d7ff5d33b9f8907ec8591bf1a899df79d
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-22 21:51:47 +0100

    Update garo.js

--- commands/garo.js
+++ commands/garo.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
-  if(!message.channel.guild) return message.reply('this command is for guilds only!');
+        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
   message.channel.send('ok').then(msg => {
     setTimeout(() => {
       msg.edit(`🚬💨💨💨`);
