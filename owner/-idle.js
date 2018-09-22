d6fd949029d0d8f9f850b95e8e5253c7ff7f2f8e
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-22 22:09:41 +0100

    Update -idle.js

--- owner/-idle.js
+++ owner/-idle.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
-  if(!message.channel.guild) return message.reply('this command is for guilds only!');
-    if (message.author.id !== '406192153979518976') return message.reply('sorry but this command for owner 😪')
+  let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+  let customemoji1 = client.emojis.find(r => r.name === '5458458469494794');
+  if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
+    if (message.author.id !== '406192153979518976') return ;
     client.user.setStatus("idle")
-message.channel.send(`Done changed to **idle** 🧡`).then(message => {message.delete(20000)})
+message.channel.send(`Done changed to **idle** ${customemoji1}`).then(message => {message.delete(20000)})
 }
