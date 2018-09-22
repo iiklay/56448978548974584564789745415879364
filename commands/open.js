39dc9720af28d090d64605f81d8ce8af47171998
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-22 21:45:55 +0100

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -1,20 +1,22 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-exports.run = (client, message, args) => {
-  if(!message.channel.guild) return message.reply('this command is for guilds only!');
-  if (message.channel.type === "dm") return;
-  if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return message.reply("You do not have Permission on this server").catch(console.error);
-  if(message.mentions.users.size === 0) {
-    return message.channel.send("You did not mention any users");
-  }
-  let unmuteMember = message.guild.member(message.mentions.users.first());
-  if(!unmuteMember) {
-    return message.channel.send("I'm not sure this user exists...");
-  }
-  if(!message.guild.member(client.user).hasPermission("MANAGE_GUILD")) {
-    return message.reply("I do not have permission to unmute...").catch(console.error);
-  }
-  message.channel.overwritePermissions(unmuteMember, { SEND_MESSAGES: true }).then(member => {
-      message.channel.send(`**${unmuteMember.user.username}** You can talk now **#${message.channel.name}** 😀`)
-})
-}
+      const Discord = require("discord.js");
+      const client = new Discord.Client();
+      exports.run = (client, message, args) => {
+        message.delete(11000);
+        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
+        if (message.channel.type === "dm") return;
+        if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return message.reply("You do not have Permission on this server").catch(console.error);
+        if(message.mentions.users.size === 0) {
+          return message.channel.send("You did not mention any users");
+        }
+        let unmuteMember = message.guild.member(message.mentions.users.first());
+        if(!unmuteMember) {
+          return message.channel.send("I'm not sure this user exists...");
+        }
+        if(!message.guild.member(client.user).hasPermission("MANAGE_GUILD")) {
+          return message.reply("I do not have permission to unmute...").catch(console.error);
+        }
+        message.channel.overwritePermissions(unmuteMember, { SEND_MESSAGES: true }).then(member => {
+            message.channel.send(`**${unmuteMember.user.username}** You can talk now **#${message.channel.name}** 😀`)
+      })
+      }
