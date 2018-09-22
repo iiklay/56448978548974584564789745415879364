8d03112b8d63e0513576176a3cdf15520d5b3255
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-22 21:52:36 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,6 @@
 exports.run = (client, message, args) => {
-  if(!message.channel.guild) return message.reply('This command only for servers ⁉');
+        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
     let start = Date.now(); message.channel.send('pong').then(message => { 
       message.edit(`Ping: ${client.ping.toFixed(0)}ms | Websocket: ${Date.now() - start}ms`);
   });
