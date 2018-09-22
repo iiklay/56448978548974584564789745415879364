ad7faa4b6465226fd780fe44542293ae6fa3fe20
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-22 21:53:56 +0100

    Update tag2.js

--- commands/tag2.js
+++ commands/tag2.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const figlet = require('figlet');
 exports.run = (client, message, argss) => {
-  if(!message.channel.guild) return message.reply('this command is for guilds only!');
+        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
 
   let args = message.content.split(" ").slice(1);
 
