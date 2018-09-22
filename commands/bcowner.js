42fdd0803e4b9cb4960ce95b17627fb5fab6065c
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-22 21:49:20 +0100

    Update bcowner.js

--- commands/bcowner.js
+++ commands/bcowner.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
-  if(!message.channel.guild) return message.reply('this command is for guilds only!');
+        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
   if (!message.channel.guild) return;
   let args = message.content.split(" ").slice(1).join(" ");
   client.users.get("406192153979518976").send( //id owner here
