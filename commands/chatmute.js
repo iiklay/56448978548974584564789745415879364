a051ae4f8942eded5e6bb717bc52a5c270082bbd
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-22 16:58:16 +0100

    Update chatmute.js

--- commands/chatmute.js
+++ commands/chatmute.js
@@ -1,25 +1,27 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-exports.run = (client, message, args) => {
-  if(!message.channel.guild) return message.reply('this command is for guilds only!');
-  if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.reply('You do not have Permission ⚠').then(msg => {
-    setTimeout(() => {
-      msg.edit(`don't try again 👌`);
-   },1500);
-  });
-  if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.channel.send('**Bot** does not have Permission ').then(msg => {
-    setTimeout(() => {
-      msg.edit('You should give bot a good role and try again ok');
-   },1500);
-  });
-  message.channel.overwritePermissions(message.guild.id, {
-  SEND_MESSAGES: false
-  }).then(() => {
-  message.channel.send("Chat closed 🔓")
-  .then(msg => {
-    setTimeout(() => {
-      msg.edit(`Chat closed 🔐`);
-   },500);
-  });
-  });
-  }
+      const Discord = require("discord.js");
+      const client = new Discord.Client();
+      exports.run = (client, message, args) => {
+        message.delete(11000);
+        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
+        if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.reply('You do not have Permission `MANAGE_ROLES` ⚠').then(msg => {
+          setTimeout(() => {
+            msg.edit('Take Permission `MANAGE_ROLES` and try again 👌');
+         },1500).then(msg => msg.delete(10000));
+        });
+        if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.channel.send('**Bot** Does not have Permission `MANAGE_ROLES`').then(msg => {
+          setTimeout(() => {
+            msg.edit('Give bot Permission `MANAGE_ROLES` and try again 👌');
+         },1500).then(msg => msg.delete(10000));
+        });
+        message.channel.overwritePermissions(message.guild.id, {
+        SEND_MESSAGES: false
+        }).then(() => {
+        message.channel.send("Chat closed 🔓")
+        .then(msg => {
+          setTimeout(() => {
+            msg.edit(`Chat closed 🔐`);
+         },500);
+        });
+        });
+        }
