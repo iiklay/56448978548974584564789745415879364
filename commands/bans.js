fa3c36864d886876a14401d896a319332635f0f0
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-22 21:49:03 +0100

    Update bans.js

--- commands/bans.js
+++ commands/bans.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
-  if(!message.channel.guild) return message.reply('this command is for guilds only!');
+        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
     if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply('You do not have Permission `ADMINISTRATOR`⚠').then(message => message.delete(35000));
     message.guild.fetchBans()
     .then(bans => message.channel.send(`\n\n\`\`\`autohotkey
