97405321bdae885bf02a098dce94a4efbd77a3d4
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-22 21:53:27 +0100

    Update shut.js

--- commands/shut.js
+++ commands/shut.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
-  if(!message.channel.guild) return message.reply('this command is for guilds only!');
+        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
     if (message.channel.type === "dm") return;
     if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return message.reply("You do not have Permission on this server").catch(console.error);
     if(message.mentions.users.size === 0) {
