fe215de167f6e0e812b0131bb051d844f0f4d2bd
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-25 07:19:00 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,6 +1,16 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const cooldown = new Set()
 exports.run = (client, message, args) => {
+          if (cooldown.has(message.author.id)) {
+    return message.reply("Please wait 5 seconds a next command").then(message => {
+     message.delete(5000) 
+    })
+    }
+    cooldown.add(message.author.id);
+    setTimeout(() => {
+        cooldown.delete(message.author.id);
+    }, 10000);
         if(!message.channel.guild) return message.reply('this command is for guilds only!');
         let customemoji = client.emojis.find(r => r.name === '549649478945145848');
         let customemoji1 = client.emojis.find(r => r.name === '5458458469494794');
