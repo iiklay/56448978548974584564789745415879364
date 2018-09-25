d0efd7193fbe4ddecba2d04dd5be78ee25c0d1c0
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-25 01:40:29 +0100

    Update text.js

--- help/text.js
+++ help/text.js
@@ -1,13 +1,19 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-exports.run = (client, message, args) => {
-        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
-        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
-  let embed = new Discord.RichEmbed()
-.setColor(message.guild.me.highestRole.color) 
-.setAuthor(message.author.username+`#${message.author.discriminator}`,message.author.displayAvatarURL)
-.addField("🈂 text commands 👏" , "tag1, tag2, tag3, tag4, mo, say, sayembed, te, emoji, mat, date")
-.setFooter(`if you want more information say -helptext`)
-.setTimestamp()
-  message.channel.sendEmbed(embed);
-}
+const send = require('quick.hook');
+exports.run = async (bot, message, args) => {
+    let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+    if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
+        let embed = new Discord.RichEmbed()
+        .setColor(message.guild.me.highestRole.color) 
+        .setAuthor(message.author.username+`#${message.author.discriminator}`,message.author.displayAvatarURL)
+        .setTitle('Dros Bot ™')
+        .setURL('https://discordapp.com/oauth2/authorize?client_id=464139412251344897&scope=bot&permissions=8')
+        .addField("Text commands" , "tag1, tag2, tag3, tag4, mo, say, sayembed, te, emoji, mat, date")
+        .setFooter(`if you want more information say -helptext`)
+        .setTimestamp()
+    send(message.channel, embed, {
+      name: 'Text CMD',
+      icon: 'https://cdn.discordapp.com/attachments/470319916537348099/493941302824796161/sscoqpy.png'
+  }).then(msg => msg.delete(30100));
+} 
