6ad0641cc4ccf4883a05a53fa1f4cd3c3313f65d
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-26 00:14:30 +0100

    Update rolesize.js

--- commands/rolesize.js
+++ commands/rolesize.js
@@ -1,5 +1,16 @@
 const Discord = require("discord.js");
+const cooldown = new Set()
 exports.run = async (client,message,args) => {
+  if(message.author.bot) return;
+  	if (cooldown.has(message.author.id)) {
+return message.reply("Please wait 5 seconds a next command").then(message => {
+message.delete(5000) 
+})
+}
+cooldown.add(message.author.id);
+setTimeout(() => {
+  cooldown.delete(message.author.id);
+}, 10000);
   var rol = message.content.split(" ").slice(1).join(" ");
   let role = message.guild.roles.find("name", `${rol}`)
   if(!role) return message.channel.send("i can not find any role");
