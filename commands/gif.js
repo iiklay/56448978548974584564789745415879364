48724cd0e87a640cf3e39596d72faa86916f44c6
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-26 00:13:37 +0100

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -1,7 +1,17 @@
 const Discord = require("discord.js");
 const gifSearch = require("gif-search");
+const cooldown = new Set()
 exports.run = (bot, message, argss) => {
   if (message.author.bot) return;
+  	if (cooldown.has(message.author.id)) {
+return message.reply("Please wait 5 seconds a next command").then(message => {
+message.delete(5000) 
+})
+}
+cooldown.add(message.author.id);
+setTimeout(() => {
+  cooldown.delete(message.author.id);
+}, 10000);
   if (message.channel.type == "dm") return;
   let args = message.content.split(' ').slice(1); 
     if (!args[0]) return message.channel.send("-gif <gname>");
