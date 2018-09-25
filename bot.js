16e2f13f538a6bc5699eccc857839f2871e2659d
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-25 23:29:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,44 +110,38 @@ client.on("message", message => {
 
 
 client.on('message', async message => {
-    if (message.content.startsWith(`-setrole`)) {
+    if (message.content.startsWith(`ff`)) {
+        if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("You need Permission `MANAGE_ROLES`");
+        let otorol = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
         let args = message.content.split(' ').slice(1); 
-if (!message.member.hasPermission("MANAGE_ROLES")) 
-return message.reply("Bu Komutu Kullanmak İçin `Rolleri Yönet` Yetkisine sahip olmalısın.").then(message => message.delete(10000));
-if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) 
-return message.reply('**Bot** does not have Permission `MANAGE_ROLES`').then(message => message.delete(10000));
-
-if(!args[1])highestRole.position
-return message.reply(`I cannot remove muted for ${userM.user.username} because high role`).then(msg => msg.delete(10000));
-let otorol = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
-if (!args[0]) { 
-    otorol[message.guild.id] = {
-        role: 0
-    };
-    fs.writeFile("./otorol.json", JSON.stringify(otorol), (err) => {
-        if (err) console.log(err);
-    });
-    message.reply("Lütfen bir rol ismi yazın.");
-}
-if (args[0]) { 
-    let roles = args.join(" ");
-    let role = message.guild.roles.find("name", roles);
-    otorol[message.guild.id] = {
-        role: role.id
-    };
-    fs.writeFile("./otorol.json", JSON.stringify(otorol), (err) => {
-        if (err) console.log(err)
-    });
-    message.reply(`seting to **${role.name}**`);
-}
-}
-client.on("guildMemberAdd", member => {
-let otorol = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
-
-var role = otorol[member.guild.id].role;
-const rol = member.guild.roles.find('name', role);
-if (!rol)
-member.addRole(role);
+        if (!args[0]) { 
+            otorol[message.guild.id] = {
+                role: 0
+            };
+            fs.writeFile("./otorol.json", JSON.stringify(otorol), (err) => {
+                if (err) console.log(err);
+            });
+            message.reply("Specify a role!");
+       }
+        if (args[0]) { 
+            let roles = args.join(" ");
+            let role = message.guild.roles.find("name", roles);
+            otorol[message.guild.id] = {
+                role: role.id
+            };
+            fs.writeFile("./otorol.json", JSON.stringify(otorol), (err) => {
+                if (err) console.log(err)
+            });
+            message.reply(`**${role.name}** role has been setting to auto role`);
+        }
+    }
+    client.on("guildMemberAdd", member => {
+        let otorol = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
+      
+        var role = otorol[member.guild.id].role;
+      const rol = member.guild.roles.find('name', role);
+        if (!rol)
+        member.addRole(role);
 })
 });
 
