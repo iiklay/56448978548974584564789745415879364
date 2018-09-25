e584d7e71bcaf959537814a853c8c4c21da9d907
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-26 00:12:46 +0100

    Update serverpic.js

--- commands/serverpic.js
+++ commands/serverpic.js
@@ -1,25 +1,26 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js')
 const client = new Discord.Client();
 const cooldown = new Set()
-exports.run = (client, message, args) => {
-          if(message.author.bot) return;
-          if (cooldown.has(message.author.id)) {
-    return message.reply("Please wait 5 seconds a next command").then(message => {
-     message.delete(5000) 
-    })
-    }
-    cooldown.add(message.author.id);
-    setTimeout(() => {
-        cooldown.delete(message.author.id);
-    }, 10000);
-        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
-        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
-  const embed = new Discord.RichEmbed()
-  .setTitle(`Photo: ${message.guild.name}`)
-  .setAuthor(message.author.tag, message.author.avatarURL)
-  .setColor(message.guild.me.highestRole.color) 
-  .setImage(message.guild.iconURL)
-  .setURL(message.guild.iconrURL)
-  .setFooter(`Requested by ${message.author.username}`)
- message.channel.send({embed});
+exports.run = async (bot, message, args) => {
+	if(message.author.bot) return;
+	if (cooldown.has(message.author.id)) {
+return message.reply("Please wait 5 seconds a next command").then(message => {
+message.delete(5000) 
+})
+}
+cooldown.add(message.author.id);
+setTimeout(() => {
+  cooldown.delete(message.author.id);
+}, 10000);
+    let msg = await message.channel.send("Wait....")
+    if(!message.guild.iconURL) return msg.edit("No icon found, you sure he is there?")
+    await message.channel.send({files: [
+    	{
+    		attachment: message.guild.iconURL,
+    		name: "servericon.png"
+    	}
+    	]})
+
+
+    msg.delete();
 }
