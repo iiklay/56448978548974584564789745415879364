a98891d892ca59db846d3bf51fe07cb8f008dc5a
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-25 21:09:17 +0100

    Update helpinfo.js

--- help/helpinfo.js
+++ help/helpinfo.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
+        if(message.author.bot) return;
         let customemoji = client.emojis.find(r => r.name === '463763583864406056');
         if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
   message.react('✉')
