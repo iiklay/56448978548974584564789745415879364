c4d2e0ba4b247efd92394486f91e1ed26516aa99
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-25 01:40:48 +0100

    Update music.js

--- help/music.js
+++ help/music.js
@@ -1,13 +1,19 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-exports.run = (client, message, args) => {
-        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
-        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
-  let embed = new Discord.RichEmbed()
-.setColor(message.guild.me.highestRole.color) 
-.setAuthor(message.author.username+`#${message.author.discriminator}`,message.author.displayAvatarURL)
-.addField("🎶 music commands 👏" , "play, pause, resume, stop, skip, queue, volume, about")
-.setFooter(`if you want more information say -helpmusic`)
-.setTimestamp()
-  message.channel.sendEmbed(embed);
-}
+const send = require('quick.hook');
+exports.run = (bot, message, args) => {
+    let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+    if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
+        let embed = new Discord.RichEmbed()
+        .setColor(message.guild.me.highestRole.color) 
+        .setAuthor(message.author.username+`#${message.author.discriminator}`,message.author.displayAvatarURL)
+        .setTitle('Dros Bot ™')
+        .setURL('https://discordapp.com/oauth2/authorize?client_id=464139412251344897&scope=bot&permissions=8')
+        .addField("Music commands" , "play, pause, resume, stop, skip, queue, volume, about")
+        .setFooter(`if you want more information say -helpmusic`)
+        .setTimestamp()
+    send(message.channel, embed, {
+      name: 'Music CMD',
+      icon: 'https://cdn.discordapp.com/attachments/470319916537348099/493941302824796161/sscoqpy.png'
+  }).then(msg => msg.delete(30100));
+} 
