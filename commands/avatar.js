7f6c2a4c50c8aaf2437cedaef6385e8b274b930b
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-25 03:00:59 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,21 +1,32 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-exports.run = (client, message, args) => {
-        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
-        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
-  var mentionned = message.mentions.users.first();
-  var x5bzm;
-  if(mentionned){
-  var x5bzm = mentionned;
-  } else {
-  var x5bzm = message.author;
-  
-  }
-  const embed = new Discord.RichEmbed()
-  .setColor(message.guild.me.highestRole.color) 
-  .setDescription(`this is avatar for **${x5bzm.avatarURL,x5bzm.tag}**`)
-  .setTimestamp()
-  .setImage(`${x5bzm.displayAvatarURL}`)
-  .setFooter(`BY : ${message.author.username}`)
-  message.channel.sendEmbed(embed);
-   }
+const sm = require('string-similarity'); 
+exports.run = (client, message, argss,) => { 
+var args = message.content.split(' ').slice(1); 
+let members = [];
+let indexes = [];
+message.guild.members.forEach(function(member){
+  members.push(member.user.tag + member.user.id)
+  indexes.push(member.id);
+})
+let match = sm.findBestMatch(args.join(' '), members);
+let username = match.bestMatch.target;
+let member = message.guild.members.get(indexes[members.indexOf(username)])
+let usernameUser = '';
+let UserMention = '';
+
+if(!args[0]) {
+  usernameUser = message.author
+  UserMention = message.member
+} else {
+let mention = message.mentions.users.first()
+  usernameUser = mention || member.user
+  UserMention = message.mentions.members.first() || message.guild.members.get(args[0]) || member
+}
+message.channel.send({embed: new Discord.RichEmbed()
+.setDescription(`this is avatar for **${usernameUser.tag}**`)
+.setColor(message.guild.me.highestRole.color) 
+.setImage(usernameUser.displayAvatarURL)
+.setFooter(`BY : ${message.author.tag}`)
+})
+};
