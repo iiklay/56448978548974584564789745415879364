0e3481c9674276fb15520272b2d404151d2328e5
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-25 07:27:00 +0100

    Update punch.js

--- gif/punch.js
+++ gif/punch.js
@@ -1,6 +1,16 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const cooldown = new Set()
 exports.run = (client, message, args) => {
+          if (cooldown.has(message.author.id)) {
+    return message.reply("Please wait 5 seconds a next command").then(message => {
+     message.delete(5000) 
+    })
+    }
+    cooldown.add(message.author.id);
+    setTimeout(() => {
+        cooldown.delete(message.author.id);
+    }, 10000);
         let customemoji = client.emojis.find(r => r.name === '463763583864406056');
         if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
   let user = message.mentions.users.first();
