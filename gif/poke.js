ed63483c797dc37084959f3d0ca0c9cec56a3454
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-25 21:28:04 +0100

    Update poke.js

--- gif/poke.js
+++ gif/poke.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const cooldown = new Set()
 exports.run = (client, message, argss) => {
+          if(message.author.bot) return;
           if (cooldown.has(message.author.id)) {
     return message.reply("Please wait 5 seconds a next command").then(message => {
      message.delete(5000) 
