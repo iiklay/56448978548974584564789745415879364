095e94ce6bd9ecab65887a83c028a23bdc66a6bb
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-27 00:19:02 +0100

    Update ²².js

--- help/²².js
+++ help/²².js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 exports.run = (client, message, args) => {
         let customemoji = client.emojis.find(r => r.name === '463763583864406056');
         if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
-   if (message.author.id !== '406192153979518976') return message.reply('sorry but this command for owner 😪')
+   if (message.author.id !== '406192153979518976') return ;
 message.delete();
    message.channel.send(message.content.split(" ").join(" ").slice(5));     
 };
