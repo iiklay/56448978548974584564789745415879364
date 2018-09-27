0e0cb1fea3109e58bf4f2aaeb511888d5d15df7b
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-27 02:01:15 +0100

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,6 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const cooldown = new Set()
+const { 
+  version 
+} = require("discord.js");
+const moment = require("moment");
+const m = require("moment-duration-format");
+let os = require('os')
+let cpuStat = require("cpu-stat")
+const ms = require("ms")
 exports.run = (client, message, args) => {
           if(message.author.bot) return;
           if (cooldown.has(message.author.id)) {
@@ -14,23 +22,31 @@ exports.run = (client, message, args) => {
     }, 10000);
         let customemoji = client.emojis.find(r => r.name === '463763583864406056');
         if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
-  if (message.author.id !== '406192153979518976') return message.reply('sorry but this command for owner 😪');
-    message.channel.send({
-      embed: new Discord.RichEmbed()
-     .setAuthor(client.user.username,client.user.avatarURL)
-     .setThumbnail(client.user.displayAvatarURL + ".png")
-     .setColor(message.guild.me.highestRole.color) 
-     .setTitle(`Information About ${client.user.username}`)
-     .addField('Ping The bot' , `${Date.now() - message.createdTimestamp}` + 'MS', true)
-     .addField('RAM Usage', `${(process.memoryUsage().rss / 1048576).toFixed()}MB`, true)
-     .addField('Guilds', client.guilds.size, true)
-     .addField('Users' ,` ${client.users.size} ` , true)
-     .addField('My Name' , ` ${client.user.tag} ` , true)
-     .addField('My ID' , ` ${client.user.id} ` , true)
-     .addField('Voice Connections' ,`${client.voiceConnections.size}` , true)
-     .addField('My Prefix' , `[ - ]` , true)
-     .addField('My Language' , `[ Java Script ]` , true)
-     .setFooter('Creator ID: abdo#1889')
-     .setTimestamp()
-  })
-  }
+  let cpuLol;
+  cpuStat.usagePercent(function(err, percent, seconds) {
+      if (err) {
+          return console.log(err);
+      }
+  const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+
+    message.channel.send(`\`\`\`asciidoc
+[Bot Stats]
+= Ping             :: ${Date.now() - message.createdTimestamp} MS
+= Guilds           :: ${Math.ceil(client.guilds.size)}
+= Users            :: ${client.users.size}
+= Channels         :: ${client.channels.size}
+= Prefix           :: [ - ]
+= RAM Usage        :: ${(process.memoryUsage().rss / 1048576).toFixed()}MB
+[About Bot]
+= Name             :: ${client.user.tag}
+= Node.js Version  :: ${process.version}
+= ID               :: ${client.user.id}
+[Host Stats]
+= Uptime           :: ${duration}
+= CPU              :: MD ${os.cpus().map(i => `${i.model}`)[0]}
+= CPU usage        :: ${percent.toFixed(2)}%
+= Platform         :: ${os.platform()}
+
+= Creator          :: abdo#1889\`\`\``)
+})
+}
