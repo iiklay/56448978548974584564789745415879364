8216fd2a7a00a72f258a897b0e77e8b62ae39311
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-27 04:48:06 +0100

    Update softban.js

--- commands/softban.js
+++ commands/softban.js
@@ -1,18 +1,7 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client();
-const sql = require("sqlite");
-sql.open("./assets/guildsettings.sqlite");
-exports.run = (client, message, args) => {
-  sql.get(`SELECT * FROM scores WHERE guildId ="${message.guild.id}"`).then(row => {
+const client = new Discord.Client();
+exports.run = async (client, message, args) => {
     let args = message.content.split(' ').slice(1); 
-  const prefixtouse = row.prefix
-  const usage = new Discord.RichEmbed()
-            .setColor(0x00A2E8)
-            .setThumbnail(client.user.avatarURL)
-            .setTitle("Command: " + prefixtouse + "softban")
-            .addField("Usage", prefixtouse + "softban @Someone <reason>")
-            .addField("Example", prefixtouse + "softban @Someone trying to start trouble.")
-            .setDescription("Description: " + "Bans and unbans a user from the current server");
 
  if (message.member.hasPermission("KICK_MEMBERS")) {
     if (!message.guild.member(client.user).hasPermission('KICK_MEMBERS')) return message.reply('Sorry, i dont have the perms to do this cmd i need KICK_MEMBERS. :x:')
@@ -26,18 +15,7 @@ exports.run = (client, message, args) => {
     message.channel.send("***The User has been successfully been soft banned! :white_check_mark:***")
     message.guild.ban(user, 2);
     message.guild.unban(user, 2);
-    sql.run(`UPDATE scores SET casenumber = ${row.casenumber + 1} WHERE guildId = ${message.guild.id}`);
-    const embed = new Discord.RichEmbed()
-    .setColor(0x00A2E8)
-    .setTitle("Case #" + row.casenumber + " | Action: Soft Ban")
-    .addField("Moderator", message.author.tag + " (ID: " + message.author.id + ")")
-    .addField("User", user.user.tag + " (ID: " + user.user.id + ")")
-    .addField("Reason", reason, true)
-    .setFooter("Time used: " + message.createdAt.toDateString())
-      if (!modlog) return;
-      if (row.logsenabled === "disabled") return;
-    return client.channels.get(modlog.id).send({embed});
-    }
-  })
+    
+  }
 }
    
