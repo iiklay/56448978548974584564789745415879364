467253fef8b33fd4fab33703970e52579e81cefb
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-28 01:25:28 +0100

    Update help.js

--- help/help.js
+++ help/help.js
@@ -15,12 +15,12 @@ exports.run = (client, message, args) => {
         let customemoji = client.emojis.find(r => r.name === '463763583864406056');
         if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
   message.delete()
-    let customemoji6 = client.emojis.find(r => r.name === '846854654886464186');
-    let customemoji1 = client.emojis.find(r => r.name === '5449874894189564');
-    let customemoji2 = client.emojis.find(r => r.name === '547984894654964564');
-    let customemoji3 = client.emojis.find(r => r.name === '54848408689447894');
-    let customemoji4 = client.emojis.find(r => r.name === '5498478456848947897');
-    let customemoji5 = client.emojis.find(r => r.name === '5648648665461654646');
+let customemoji6 = client.emojis.find(r => r.name === 'arabic');
+let customemoji1 = client.emojis.find(r => r.name === '5449874894189564');
+let customemoji2 = client.emojis.find(r => r.name === 'info');
+let customemoji3 = client.emojis.find(r => r.name === 'admin');
+let customemoji4 = client.emojis.find(r => r.name === 'music');
+let customemoji5 = client.emojis.find(r => r.name === 'text');
   let abdo = new Discord.RichEmbed()
   .setColor(message.guild.me.highestRole.color) 
   .setAuthor(message.author.username+`#${message.author.discriminator}`,message.author.displayAvatarURL)
