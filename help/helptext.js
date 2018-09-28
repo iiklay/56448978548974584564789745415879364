de806222d518a333fdf59c4b1bcd7922fe9256ab
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-28 03:22:36 +0100

    Update helptext.js

--- help/helptext.js
+++ help/helptext.js
@@ -1,28 +1,28 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
-        if(message.author.bot) return;
-        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
-        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
-  message.react('✉')
-  message.reply('bot is been sent in private with the information').then(message => {message.delete(20000)})
-message.author.send(`\n\n\`\`\`
-              🈂 text commands 👏   
-_________________________________________________
-|                                               |
-| -tag1 Write with color yellow                 |
-| -tag2 Write with color normal                 |
-| -tag3 Write with color blue                   |
-| -tag4 Write with color green                  |
-| -mo writ tweet                                |
-| -say command say normal                       |
-| -sayembed command say withe emebed            |
-| -te Write in a beautiful way                  |
-| -mat Mathematics same 1+1=2                   |
-| -date to show date                            |
-| -emoji Convert words to emoji                 |
-|_______________________________________________|
-                                             
-we will add more commands a soon to contact the owner
-    \`\`\``)
+randomNumber = Math.floor(Math.random() * (6 - 1) + 1);
+if(message.author.bot) return;
+let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+let customemoji1 = client.emojis.find(r => r.name === 'done');
+if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
+if(randomNumber==2){
+message.react('🖍')
+}else{
+message.react(`✏`)
+}
+message.reply(`${customemoji1} check from your private`).then(message => {message.delete(20000)})
+message.author.send(`\`\`\`css
+= text commands =
+
+-tag1     :: Write with color yellow
+-tag2     :: Write with color normal
+-tag3     :: Write with color blue
+-tag4     :: Write with color green
+-say      :: command say normal
+-sayembed :: command say withe emebed
+-te       :: Write in a beautiful way
+-mat      :: Mathematics same 1+1=2
+-date     :: to show date
+-emoji    :: Convert words to emoji \`\`\``)
 }
