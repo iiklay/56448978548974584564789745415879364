7ecc9ddf48e492d33d01892e1a9c232e3c9d9c2e
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-28 02:59:57 +0100

    Update helpinfo.js

--- help/helpinfo.js
+++ help/helpinfo.js
@@ -1,32 +1,34 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 exports.run = (client, message, args) => {
-        if(message.author.bot) return;
-        let customemoji = client.emojis.find(r => r.name === '463763583864406056');
-        if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
-  message.react('✉')
-  message.reply('bot is been sent in private with the information').then(message => {message.delete(20000)})
-message.author.send(`\n\n\`\`\`
-               📚 fun commands 👏    
-_________________________________________________
-|                                               |
-| -bots to show all bots in server              |
-| -rooms to show all rooms in server            |
-| -count start count time                       |
-| -meteo To see the weather                     |
-| -invite invitation the bot                    |
-| -bcowner send message to owner                |
-| -serverpic show photo the server              |
-| -userinfo Information about user              |
-| -server Information about server              |
-| -avatar photo profile person                  |
-| -ping The quality of the bot connection       |
-| -myid info about your profile                 |
-| -botinfo Information about bot                |
-| -support link server support                  |
-| -uptime boot time the bot                     |
-|_______________________________________________|
-                                             
-we will add more commands a soon to contact the owner
-    \`\`\``)
+randomNumber = Math.floor(Math.random() * (6 - 1) + 1);
+if(message.author.bot) return;
+let customemoji = client.emojis.find(r => r.name === '463763583864406056');
+let customemoji1 = client.emojis.find(r => r.name === 'done');
+if(!message.channel.guild) return message.reply(`This command is for guilds only! ${customemoji}`);
+if(randomNumber==2){
+message.react('📚')
+}else{
+message.react(`🔎`)
+}
+message.reply(`${customemoji1} check from your private`).then(message => {message.delete(20000)})
+message.author.send(`\`\`\`asciidoc
+= info commands =
+
+-bots      :: to show all bots in server | لإظهار جميع البوتات في الخادم
+-rooms     :: to show all rooms in server | لإظهار جميع الغرف في الخادم
+-count     :: Alarm clock | المنبه
+-meteo     :: To see your own weather | لرؤية الطقس الخاص بك
+-invite    :: invitation the bot | دعوة البوت
+-bcowner   :: send message to owner bot | أرسل رسالة إلى المالك البوت
+-serverpic :: show photo the server | عرض صورة الخادم
+-userinfo  :: Information about user | معلومات حول المستخدم
+-server    :: Information about server | معلومات حول الخادم
+-avatar    :: photo profile the user | الصورة الشخصية للمستخدم 
+-ping      :: to see the quality of the bot connection | لرؤية جودة اتصال البوت
+-myid      :: info about your profile | معلومات عن ملفك الشخصي
+-botinfo   :: Information about bot | معلومات حول بوت
+-support   :: link server support
+-uptime    :: boot time bot | وقت اقلاع البوت
+-rolesize  :: to see info about role | للاطلاع على معلومات حول الدور | \`\`\``)
 }
