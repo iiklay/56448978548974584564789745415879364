9f0f803d14a98ec37b93bac13e64ad15f25089ba
348724478545698785444444454678545245848 <43321387+348724478545698785444444454678545245848@users.noreply.github.com>
2018-09-28 04:50:36 +0100

    Update trigger.js

--- commands/trigger.js
+++ commands/trigger.js
@@ -1,13 +1,14 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const snek = require('snekfetch');
-const fsn = require('fs-nextra');
 exports.run = async (client, message, args) => {
+  var fsn = module.require('fs-nextra');
+  let args = message.content.split(' ').slice(1); 
   if (!message.guild.member(client.user).hasPermission('ATTACH_FILES')) return message.reply('Sorry, i dont have the perms to do this cmd i need ATTACH_FILES. :x:')
    const { Canvas } = require('canvas-constructor');
     if (message.mentions.users.size < 1) return message.channel.send("You didn't mention a user to put them behind bars");
    const getSlapped = async (person) => {
-    const plate = await fsn.readFile('./assets/images/plate_triggered.gif');
+    const plate = await fsn.readFile('./assets/images/plate_triggered.png');
     const png = person.replace('.gif', '.png');
     const { body } = await snek.get(png);
     return new Canvas(330, 330)
@@ -19,7 +20,7 @@ exports.run = async (client, message, args) => {
      try {
     const person = message.mentions.users.first().avatarURL;
     const result = await getSlapped(person);
-    await message.channel.send({ files: [{ attachment: result, name: 'trigger.gif' }] });
+    await message.channel.send({ files: [{ attachment: result, name: 'trigger.png' }] });
   } catch (error) {
     throw error;
   }
